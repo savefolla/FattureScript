216d3810fa15ac556e781b70b9d69721bf78a003
Saverio Follador <saverio.follador@moku.io>
2017-11-03 18:45:52 +0100

    update ripulisci

--- RipulisciScript/script.js
+++ RipulisciScript/script.js
@@ -6,14 +6,40 @@ var fs = require('fs');
 
 var args = process.argv.slice(2);
 
-var filename = args[0];
+var from = args[0];
+var to = args[1];
 
-fs.readFile(filename, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  };
+// node script.js "data da" "data a" 
 
-  var fatts = csvToJson(data);
+var postData = {
+  "api_uid": api.UID,
+  "api_key": api.key,
+  "anno": 2017,
+  "data_inizio": from,
+  "data_fine": to,
+  "cliente": "",
+  "fornitore": "",
+  "id_cliente": "",
+  "id_fornitore": "",
+  "saldato": "",
+  "oggetto": "",
+  "ogni_ddt": "",
+  "PA": false,
+  "PA_tipo_cliente": "",
+  "pagina": 1
+}
+
+var options = {
+  method: 'post',
+  body: postData,
+  json: true,
+  url: url.fattureLista
+};
+
+request(options, function(err, res, body) {
+    
+  
+  var fatts = body.lista_documenti;
 
   var handle = function(fatts, i) {
     if(i>=fatts.length){
@@ -23,7 +49,7 @@ fs.readFile(filename, 'utf8', function (err,data) {
     var postData = {};
     postData.api_uid = api.UID;
     postData.api_key = api.key;
-    postData.id = fatts[i];
+    postData.id = fatts[i].id;
 
     var options = {
       method: 'post',
--- RipulisciScript/url.js
+++ RipulisciScript/url.js
@@ -2,3 +2,5 @@
 exports.url = "https://api.fattureincloud.it/v1";
 
 exports.fatture = this.url + "/fatture/elimina";
+
+exports.fattureLista = this.url + "/fatture/lista";
